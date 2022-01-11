0a0bc1e4f38f7e558bb806bd98a4ce6922013499
HarlG <harlan.gillespie1@gmail.com>
2022-01-11 19:50:34 +0000

    pysam vcf_reader setup

--- VCF_matrix.py
+++ VCF_matrix.py
@@ -1,1 +1,10 @@
 ## Test initial git commit ##
+from pysam import VariantFile
+
+
+vcf_filename = "ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
+panel_filename = "phase1_integrated_calls.20101123.ALL.panel"
+
+with VariantFile(vcf_filename) as vcf_reader:
+    for record in vcf_reader:
+        print(record)
