abfbe617d8a45ee2d576bc6bfcdc2b3acd98f237
HarlG <harlan.gillespie1@gmail.com>
2022-01-15 16:40:37 +0000

    Output genotypes as numpy array

--- VCF_matrix.py
+++ VCF_matrix.py
@@ -1,13 +1,24 @@
 ## Test initial git commit ##
 from pysam import VariantFile
+import numpy as np
 
 
 vcf_filename = "ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
 panel_filename = "phase1_integrated_calls.20101123.ALL.panel"
 
+
+genotypes = []
+samples = []
+
 with VariantFile(vcf_filename) as vcf_reader:
+    counter = 0
     for record in vcf_reader:
         alleles = [record.samples[x].allele_indices for x in record.samples]
         samples = [sample for sample in record.samples]
-        print(alleles)
-        print(samples)
+        genotypes.append(alleles)
+        counter += 1
+        if counter >= 100:
+            break
+        
+genotypes = np.array(genotypes)
+print(genotypes.shape)
