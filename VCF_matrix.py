95a7146575dd5d8431d962e57ba65c1533b3647d
HarlG <harlan.gillespie1@gmail.com>
2022-01-19 17:08:52 +0000

    PCA run on every 100th SNP

--- VCF_matrix.py
+++ VCF_matrix.py
@@ -1,7 +1,7 @@
 ## Test initial git commit ##
 from pysam import VariantFile
 import numpy as np
-
+from sklearn import decomposition
 
 vcf_filename = "ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
 panel_filename = "phase1_integrated_calls.20101123.ALL.panel"
@@ -13,12 +13,23 @@ samples = []
 with VariantFile(vcf_filename) as vcf_reader:
     counter = 0
     for record in vcf_reader:
-        alleles = [record.samples[x].allele_indices for x in record.samples]
-        samples = [sample for sample in record.samples]
-        genotypes.append(alleles)
-        counter += 1
-        if counter >= 100:
+        count +=1
+        if counter % 100 == 0:
+            alleles = [record.samples[x].allele_indices for x in record.samples]
+            samples = [sample for sample in record.samples]
+            genotypes.append(alleles)
+            counter += 1
+        if counter >= 10000:
             break
         
 genotypes = np.array(genotypes)
 print(genotypes.shape)
+
+matrix = np.count_nonzero(genotypes, axis =2)
+print(matrix.shape)
+
+pca = decomposition.PCA(n_components=2)
+pca.fit(matrix)
+print(pca.singular_values_)
+to_plot = pca.transform(matrix)
+print(to_plot.shape)
