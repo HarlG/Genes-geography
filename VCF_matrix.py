c97ac7eb5e0a710d8a60be5337d0a5cae80a6ec1
HarlG <harlan.gillespie1@gmail.com>
2022-02-07 12:27:31 +0000

    Added a print percentage completed function. Commenting added

--- VCF_matrix.py
+++ VCF_matrix.py
@@ -12,19 +12,28 @@ genotypes = []
 samples = []
 variant_ids = []
 
+#VCF reader
 with VariantFile(vcf_filename) as vcf_reader:
     counter = 0
+    # iterate through variants in VCF
     for record in vcf_reader:
         counter +=1
+        # if statement below only reads every 100 variants to speed up program.
+        # This should not disrupt results due to linkage disequilibrium
+        # Can be commented out if whole VCF file to be read. Will be option later when args parse functionality added.
         if counter % 100 == 0:
+            # Collects allele and sample information
             alleles = [record.samples[x].allele_indices for x in record.samples]
             samples = [sample for sample in record.samples]
             genotypes.append(alleles)
             variant_ids.append(record.id)
-        if counter >= 10000:
-            break
+        # Print percent completed as program runs
+        if counter % 4943 == 0:
+            print(f'Completed: {round(100 * counter/494328)}%')
+        #if counter >= 10000:
+        #    break
 
-
+# Panel file reader i.e. population codes
 with open(panel_filename) as panel_file:
     labels = {} #{sample_id: population code}
     for line in panel_file:
@@ -41,12 +50,14 @@ matrix = np.count_nonzero(genotypes, axis =2)
 matrix = matrix.T
 print(matrix.shape)
 
-pca = decomposition.PCA(n_components=2)
-pca.fit(matrix)
-print(pca.singular_values_)
-to_plot = pca.transform(matrix)
-print(to_plot.shape)
+#pca = decomposition.PCA(n_components=2)
+#pca.fit(matrix)
+#print(pca.singular_values_)
+#to_plot = pca.transform(matrix)
+#print(to_plot.shape)
 
+
+# Data put in dataframe. Outputs as CSV
 df = pd.DataFrame(matrix, columns = variant_ids, index = samples)
 df['Population code'] = df.index.map(labels)
 df.to_csv("matrix.csv")
